7bb8c4fecbd353750413bb5de1241c177ef67c2b
FilipKisic <filip.kisic@racunarstvo.hr>
2023-12-08 19:35:47 +0100

    better UI

--- src/app/presentation/page/auth/LoginPage.js
+++ src/app/presentation/page/auth/LoginPage.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import { Button, Form } from "react-bootstrap";
+import { Button, Form, Container, Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { login } from "../../redux/actions/authActions";
 
-const LoginPage = ({ history }) => {
+const LoginPage = () => {
   const dispatch = useDispatch();
   const auth = useSelector((state) => state.authReducer);
   const navigate = useNavigate();
@@ -17,44 +17,47 @@ const LoginPage = ({ history }) => {
     dispatch(login(email, password));
   };
 
-  // Redirect to the home page when the login is successful
   React.useEffect(() => {
     if (auth.token) {
       navigate("/home");
     }
-  }, [auth.token, history]);
+  }, [auth.token]);
 
   return (
-    <div className="login-container">
-      <h2>Login</h2>
-      <Form onSubmit={handleLogin}>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </Form.Group>
+    <Container>
+      <Row className="justify-content-md-center mt-5">
+        <Col xs={12} md={6}>
+          <Form onSubmit={handleLogin}>
+            <h2 className="mb-4">Login</h2>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Form.Group>
 
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
 
-        <Button variant="primary" type="submit">
-          Login
-        </Button>
-      </Form>
+            <Button variant="primary" type="submit" className="mt-3">
+              Login
+            </Button>
+          </Form>
 
-      {auth.error && <div className="error-message">{auth.error}</div>}
-    </div>
+          {auth.error && <div className="error-message mt-3">{auth.error}</div>}
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
