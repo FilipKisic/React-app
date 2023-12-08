16bace6237c4c518a9e9e342f839e9dd65d2e91e
FilipKisic <filip.kisic@racunarstvo.hr>
2023-12-08 19:23:13 +0100

    Should work, almost there

--- src/app/presentation/page/auth/LoginPage.js
+++ src/app/presentation/page/auth/LoginPage.js
@@ -1,73 +1,58 @@
-import React, { useState, useEffect } from "react";
-import { Form, Button, Container, Row, Col } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { login } from "../../redux/actions/authActions";
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { login } from '../../redux/actions/authActions';
 
-import "./LoginPage.css";
-
-const LoginPage = () => {
-  console.log("LoginPage rendered");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
-
+const LoginPage = ({ history }) => {
   const dispatch = useDispatch();
-  const token = useSelector((state) => state.auth.token || null);
-  const error = useSelector((state) => state.auth.error || null);
+  const auth = useSelector((state) => state.authReducer);
 
-  const handleLogin = (e) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (e) => {
     e.preventDefault();
-
     dispatch(login(email, password));
   };
 
-  useEffect(() => {
-    if (token !== null && token !== undefined) {
-      navigate("/home");
+  // Redirect to the home page when the login is successful
+  React.useEffect(() => {
+    if (auth.token) {
+      history.push('/home'); // Change '/home' to the actual route you want to redirect to
     }
-  }, [token, navigate]);
-
-  useEffect(() => {
-    if (error !== null && error !== undefined) {
-      console.log("Error happend:", error);
-    }
-  }, [error]);
+  }, [auth.token, history]);
 
   return (
-    <Container>
-      <Row className="justify-content-md-center mt-5">
-        <Col xs={12} md={6}>
-          <Form className="login-form" onSubmit={handleLogin}>
-            <h2 className="mb-4">Login</h2>
-            <Form.Group class>
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Enter email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </Form.Group>
-            <Button className="submit-button" variant="primary" type="submit">
-              Login
-            </Button>
-            {error && <h3 className="text-danger mt-3">{error}</h3>}
-          </Form>
-        </Col>
-      </Row>
-    </Container>
+    <div className="login-container">
+      <h2>Login</h2>
+      <Form onSubmit={handleLogin}>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Login
+        </Button>
+      </Form>
+
+      {auth.error && <div className="error-message">{auth.error}</div>}
+    </div>
   );
 };
 
